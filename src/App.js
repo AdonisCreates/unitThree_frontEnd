f60804fb60691d2098ba1da9899ad5e1fe353a63
amacken <alex.mackenzie2@gmail.com>
2020-09-05 14:52:11 -0400

    Styling branch created. Extra files deleted. Basic html displaying on page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from "react-router-dom";
+import axios from "axios";
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to our Project #3
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
