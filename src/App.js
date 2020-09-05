25b82664d2d3ed77c159a75e19a9c34c79dcbbfe
amacken <alex.mackenzie2@gmail.com>
2020-09-05 16:12:55 -0400

    added handleLogOut, handleInput, handleSignUp, and handleLogIn to the App.js file. NavBar now showing up on page, but other routes error out currently so they are commented out

--- src/App.js
+++ src/App.js
@@ -9,16 +9,121 @@ import LogInForm from "./components/LogInForm/LogInForm";
 import LogOut from "./components/LogOut/LogOut";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+    isLoggedIn: false,
+  });
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, [isLoggedIn]);
+
+  const handleLogOut = () => {
+    setState({
+      email: "",
+      password: "",
+      isLoggedIn: false,
+    });
+    localStorage.clear();
+  };
+
+  const handleInput = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/users/signup", {
+        email: state.email,
+        password: state.password,
+      });
+      console.log(response);
+      localStorage.token = response.data.token;
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogIn = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3001/users/login", {
+        email: state.email,
+        password: state.password,
+      });
+      localStorage.token = response.data.token;
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-        <p>
-          Welcome to our Project #3
-        </p>
-      </header>
-    </div>
+    <>
+      <div>
+        <NavBar isLoggedIn={isLoggedIn} />
+        <div className="body">
+          {/* <Switch>
+            <Route
+              path="/signup"
+              render={(props) => {
+                return (
+                  <SignUpForm
+                    isLoggedIn={isLoggedIn}
+                    handleInput={handleInput}
+                    handleSignUp={handleSignUp}
+                  />
+                );
+              }}
+            />
+            <Route
+              path="/logout"
+              render={(props) => {
+                return (
+                  <LogOut isLoggedIn={isLoggedIn} handleLogOut={handleLogOut} />
+                );
+              }}
+            />
+            <Route
+              path="/login"
+              render={(props) => {
+                return (
+                  <LogInForm
+                    isLoggedIn={isLoggedIn}
+                    handleInput={handleInput}
+                    handleLogIn={handleLogIn}
+                  />
+                );
+              }}
+            />
+            <Route
+              path="/"
+              render={() => {
+                return <PlaylistList isLoggedIn={isLoggedIn} />;
+              }}
+            />
+          </Switch> */}
+        </div>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello World!</h1>
+          <p>
+            Welcome to our Project #3
+          </p>
+        </header>
+      </div>
+    </>
   );
 }
 
