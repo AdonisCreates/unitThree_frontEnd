2e232f5bb399016b651bfb5136988ee41d476faf
amacken <alex.mackenzie2@gmail.com>
2020-09-05 15:10:02 -0400

    components files added and imported to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Route, Switch } from "react-router-dom";
 import axios from "axios";
 
-
+import NavBar from "./components/NavBar/NavBar";
+import PlaylistList from "./components/PlaylistList/PlaylistList";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+import LogInForm from "./components/LogInForm/LogInForm";
+import LogOut from "./components/LogOut/LogOut";
 import './App.css';
 
 function App() {
