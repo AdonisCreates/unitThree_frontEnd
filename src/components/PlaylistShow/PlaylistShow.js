994d8cacf204f5bc0c850f1ed4497f7271a0f194
amacken <alex.mackenzie2@gmail.com>
2020-09-10 10:44:00 -0400

    added search bar to PlaylistShow page. added stlying to search bar

--- src/components/PlaylistShow/PlaylistShow.js
+++ src/components/PlaylistShow/PlaylistShow.js
@@ -4,11 +4,16 @@ import "./PlaylistShow.css";
 function PlaylistShow(props) {
     const { artist, album, title } = props.playlist;
     return (
-        <div className="playlist-preview">
-            {props.isLoggedIn ? <h3>Artist: {artist}</h3> : ""}
-            {props.isLoggedIn ? <h3>Album: {album}</h3> : ""}
-            {props.isLoggedIn ? <h3>Song: {title}</h3> : ""}
-        </div>
+        <>
+            <div className="search">
+                <input type="text" id="search" name="search" placeholder="enter search here"></input>
+            </div>
+            <div className="playlist-preview">
+                {props.isLoggedIn ? <h3>Artist: {artist}</h3> : ""}
+                {props.isLoggedIn ? <h3>Album: {album}</h3> : ""}
+                {props.isLoggedIn ? <h3>Song: {title}</h3> : ""}
+            </div>
+        </>
     );
 }
 
